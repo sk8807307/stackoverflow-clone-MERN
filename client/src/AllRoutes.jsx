765a20208f9d8fbd6c0c1d2f881b55a72565c0fe
sk8807307 <sk8807307@gmail.com>
2022-12-15 12:26:23 +0530

    update

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -4,6 +4,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home/Home'
 import Auth from './components/pages/Auth/Auth'
 import Questions from './components/pages/Questions/Questions'
+import AskQuestion from './components/pages/AskQuestion/AskQuestion'
+import DisplayQuestion from './components/pages/Questions/DisplayQuestion'
 
 const AllRoutes = () => {
   return (
@@ -11,6 +13,8 @@ const AllRoutes = () => {
         <Route path='/' element={<Home />} />
         <Route path='/Auth' element={<Auth />} />
         <Route path='/Questions' element={<Questions />} />
+        <Route path='/AskQuestion' element={<AskQuestion />} />
+        <Route path='/DisplayQuestion' element={<DisplayQuestion />} />
       </Routes>
   )
 }
--- client/src/components/HomeMainbar/HomeMainbar.jsx
+++ client/src/components/HomeMainbar/HomeMainbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import QuestionsList from './QuestionsList'
+import './HomeMainbar.css'
 
 const HomeMainbar = () => {
 
-// console.log(questionsList)
+
+    const location = useLocation();
+    const user = 1;
+    const navigate = useNavigate();
+
     var questionsList = [{ 
         _id: 1,
         upVotes: 3,
@@ -23,7 +29,7 @@ const HomeMainbar = () => {
         }]
     },{ 
         _id: 2,
-        upVotes: 3,
+        upVotes: 0,
         downVotes: 2,
         noOfAnswers: 0,
         questionTitle: "What is a function?",
@@ -40,7 +46,7 @@ const HomeMainbar = () => {
         }]
     },{ 
         _id: 3,
-        upVotes: 3,
+        upVotes: 1,
         downVotes: 2,
         noOfAnswers: 0,
         questionTitle: "What is a function?",
@@ -57,13 +63,32 @@ const HomeMainbar = () => {
         }]
     }]
 
-    const location = useLocation()
+  const checkAuth = () => {
+    if(user === null){
+      alert("login or signup to ask a question")
+      navigate('/Auth')
+    }else
+    {
+      navigate('/AskQuestion')
+    }
+  }
 
   return (
     <div className='main-bar'>
       <div className='main-bar-header'>
         {
           location.pathname === '/' ? <h1>Top Questions</h1> : <h1>All Questions</h1>
+        }
+        <button onClick={checkAuth} className='ask-btn'>Ask Question</button>
+      </div>
+      <div>
+        {
+          questionsList === null ?
+          <h1>Loading...</h1> :
+          <>
+            <p>{questionsList.length} questions</p>
+            <QuestionsList questionsList={questionsList} />
+          </>
         }
       </div>
     </div>
