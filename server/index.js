887e2a2e86af6d85aed171cfad97ee2e980597d6
sk8807307 <sk8807307@gmail.com>
2022-12-20 15:51:01 +0530

    update

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express();
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: "true"}))
+app.use(cors());
+
+app.get('/',(req, res) => {
+    res.send("This is a stackoverflow clone API")
+})
+
+const PORT = process.env.PORT || 5000
+
+const {}
+mongoose.connect()
