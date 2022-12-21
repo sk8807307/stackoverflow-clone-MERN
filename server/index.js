95c40f1d040d6844d70b25dd80aa7dd099ca3b65
sk8807307 <sk8807307@gmail.com>
2022-12-21 12:00:47 +0530

    update

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+import userRoutes from './routes/users.js'
+
 const app = express();
 app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: "true"}))
@@ -11,7 +13,12 @@ app.get('/',(req, res) => {
     res.send("This is a stackoverflow clone API")
 })
 
+app.use('/user', userRoutes)
+
 const PORT = process.env.PORT || 5000
 
-const {}
-mongoose.connect()
+const CONNECTION_URL = "mongodb+srv://crexz:7711@stack-overflow-clone.kbywlkk.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => {console.log(`server is running on PORT ${PORT}`)}))
+    .catch((err) => console.log(err.message))
