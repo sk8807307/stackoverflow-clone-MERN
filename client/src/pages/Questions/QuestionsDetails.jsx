07ce68d01c5f433386676357f9c7b16eb2931d64
sk8807307 <sk8807307@gmail.com>
2023-01-23 14:37:57 +0530

    updata clientside

--- client/src/pages/Questions/QuestionsDetails.jsx
+++ client/src/pages/Questions/QuestionsDetails.jsx
@@ -72,7 +72,7 @@ const QuestionsDetails = () => {
     const dispatch = useDispatch()
     const User = useSelector((state) => (state.currentUserReducer))
     const location = useLocation()
-    const url = 'http://localhost:3000'
+    const url = 'https://clone01.onrender.com'
 
     const handlePostAns = (e, answerLength) => {
         e.preventDefault()
